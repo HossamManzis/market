bd2767a5a284c970bb47c0a2ae1f6f770ecc90b8
Apodidae <2012@unisnu.ac.id>
2023-10-27 00:25:49 +0700

    laravel user-role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,11 +15,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(20)->create();
         User::create([
             'name' =>'Hossam Manzis',
             'email' =>'hossammanziz@gmail.com',
             'email_verified_at' => now(),
+            'role' => 'admin',
+            'phone' => '6281325613670',
+            'bio' => 'Example',
             'password' => Hash::make('247'),
+        ]);
+
+        User::create([
+            'name' =>'Yami Tsubame',
+            'email' =>'yamiyami@gmail.com',
+            'email_verified_at' => now(),
+            'role' => 'superadmin',
+            'phone' => '6283869495744',
+            'bio' => 'Example 2',
+            'password' => Hash::make('1014'),
         ]);
     }
 }
